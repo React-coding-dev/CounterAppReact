ed6161df19d5eaff2e1adebfc4f91890b7016c78
React-Coding-Dev <iamnooneuf@gmail.com>
2023-12-04 00:34:11 +0500

    deploying counter app

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'; 
+import './App.css'; 
+import HelloWorld from './HelloWorld';
+import Counter from './Counter';
+
+class App extends Component { render() {
+     return (
+       <div className="App">
+         <HelloWorld name = {`[ABC XYZ]`}/>
+         <Counter />
+       </div>
+     );
+   }
+}
+export default App;
